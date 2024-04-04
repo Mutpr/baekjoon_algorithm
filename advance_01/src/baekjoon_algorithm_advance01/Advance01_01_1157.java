8db7b103484071da803f5adb5ece4bdf03889bf8
hyen <honghyen47@gmail.com>
2024-04-04 15:07:08 +0900

    0404 secondCommit

--- advance_01/src/baekjoon_algorithm_advance01/Advance01_01_1157.java
+++ advance_01/src/baekjoon_algorithm_advance01/Advance01_01_1157.java
@@ -1,5 +1,7 @@
 package baekjoon_algorithm_advance01;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Advance01_01_1157 {
@@ -11,30 +13,19 @@ public class Advance01_01_1157 {
 		String strLower = str.toLowerCase();
 
 		countA(strLower);
-		
+
 	}
 
 	public static void countA(String str) {
-		int cnt = 0;
 		char[] strToChar = str.toCharArray();
-		char[] sameChar = new char[str.length()];
-		for (int i = 1; i < strToChar.length; i++) {
-			if (strToChar[i] == strToChar[i - 1]) {
-				sameChar[i-1] = strToChar[i-1];
+		HashMap<Integer, Character> map = new HashMap<Integer, Character>();
+		for(int i=1; i<strToChar.length; i++) {
+			for(int j=0; j<i; j++) {
+				if(strToChar[i] == strToChar[j]) {
+					strToChar[i] = '\u0000';
+				}
 			}
-		}
-		for(int i=0; i<sameChar.length; i++) {
-			if(sameChar[i] != '\0') {
-				cnt++;
-			}
-		}
-		if(cnt == 1) {
-			for(int i=0; i<sameChar.length; i++) {
-				System.out.println(Character.toUpperCase(sameChar[i]));
-			}
-		}else if(cnt != 1){
-			System.out.println("?");
-		}
+		}System.out.print(strToChar);
 	}
 
 }
