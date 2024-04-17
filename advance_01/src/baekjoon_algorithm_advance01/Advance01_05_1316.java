017f0a72ee83a5dd4cc53cbdac4629c4061a3e63
hyen <honghyen47@gmail.com>
2024-04-17 16:10:19 +0900

    0417 commit

---
+++ advance_01/src/baekjoon_algorithm_advance01/Advance01_05_1316.java
@@ -0,0 +1,11 @@
+package baekjoon_algorithm_advance01;
+
+import java.util.Scanner;
+
+public class Advance01_05_1316 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
