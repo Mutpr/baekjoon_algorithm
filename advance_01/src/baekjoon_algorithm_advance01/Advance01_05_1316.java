123922bfa65e04fe59a5d5c5ff704ceb4d3a61f2
hyen <honghyen47@gmail.com>
2024-04-17 20:56:51 +0900

    0417

--- advance_01/src/baekjoon_algorithm_advance01/Advance01_05_1316.java
+++ advance_01/src/baekjoon_algorithm_advance01/Advance01_05_1316.java
@@ -6,6 +6,22 @@ public class Advance01_05_1316 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	}
+		int length = sc.nextInt();
+		int count = 0;
+		char prev = ' ';
 
+		for (int i = 0; i < length; i++) {
+			String line = sc.nextLine().toLowerCase();
+			for(int j = 0; j<i; i++) { // j의 범위를 수정합니다.
+				char character = line.charAt(j);
+				prev = character;
+				if(prev == character) {
+					count++;
+				}
+			}
+
+		}System.out.println(count);
+		
+
+	}
 }
